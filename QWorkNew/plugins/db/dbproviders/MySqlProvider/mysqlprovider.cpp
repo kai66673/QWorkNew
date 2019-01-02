90cf83b261b44a8d5b11ff21d96cf6321224af65
Alexander Kudryavtsev <kai66673@mail.ru>
2019-01-02 19:36:50 +0300

    ...'

--- QWorkNew/plugins/db/dbproviders/MySqlProvider/mysqlprovider.cpp
+++ QWorkNew/plugins/db/dbproviders/MySqlProvider/mysqlprovider.cpp
@@ -1,4 +1,5 @@
 #include "Core.h"
+#include "DbUtils.h"
 
 #include "mysqlauthwidget.h"
 #include "mysqlprovider.h"
@@ -130,11 +131,6 @@ MetadataQuery::Ptr Provider::objectTableNameMetadataQuery(const QString &schemaN
     return MetadataQuery::Ptr(nullptr);
 }
 
-IDbDetailsFactory *Provider::createDbDetailsFactory(const QString &connName, const QString &schemaName, int schemaObjectType, int schemaObjectSubtype, const QString &objName) const
-{
-    return nullptr;
-}
-
 QStringList Provider::additionalSchemaCategories() const
 {
     return QStringList() << QObject::tr("Routines");
@@ -158,6 +154,228 @@ QString Provider::additionalSchemaObjectIconName(int schemaObjectType, ItemPrope
     }
 
     return QStringLiteral("");
+}
+
+static IDbDetailsFactory *createTableDbDetailsFactory(const QString &connName, const QString &schemaName, const QString &tableName)
+{
+    QSqlDatabase db = QSqlDatabase::database(connName);
+
+    if ( !db.isOpen() ) {
+        Core::Storage::warning(QObject::tr("Database connection is not opened."));
+        return nullptr;
+    }
+
+    // Table Info
+    Database::DbDetailsFormViewFactory *t = new Database::DbDetailsFormViewFactory;
+    {
+        t->addItem(Database::DbDetailsFormViewFactory::FormItem(QObject::tr("Name"), tableName));
+        QSqlQuery query(db);
+        QString   queryString;
+        queryString = QString("SELECT TABLE_COMMENT FROM INFORMATION_SCHEMA.TABLES "
+                              " WHERE TABLE_SCHEMA = '%1' "
+                              "   AND TABLE_NAME = '%2'").arg(schemaName).arg(tableName);
+        QString description = "";
+        if (Database::Utils::executeQueryWithLog(&query, queryString))
+            if ( query.next() )
+                description = query.value(0).toString();
+        t->addItem(Database::DbDetailsFormViewFactory::FormItem(QObject::tr("Description"), description));
+    }
+
+    // Table Columns Info
+    Database::DbDetailsTableFactory *c = new Database::DbDetailsTableFactory(QStringList() << QObject::tr("Name")
+                                                                                           << QObject::tr("Description")
+                                                                                           << QObject::tr("Data Type")
+                                                                                           << QObject::tr("Nullable") );
+    {
+        QSqlQuery query(db);
+        QString   queryString;
+        queryString = QString("SELECT COLUMN_NAME, COLUMN_COMMENT, COLUMN_TYPE, IS_NULLABLE FROM INFORMATION_SCHEMA.COLUMNS "
+                              " WHERE TABLE_SCHEMA = '%1' "
+                              "   AND TABLE_NAME = '%2' "
+                              " ORDER BY ORDINAL_POSITION").arg(schemaName).arg(tableName);
+        if (Database::Utils::executeQueryWithLog(&query, queryString))
+            while (query.next()) {
+                QStringList row;
+                row << query.value(0).toString();
+                row << query.value(1).toString();
+                row << query.value(2).toString();
+                row << query.value(3).toString();
+                c->addRow(row);
+            }
+    }
+
+    // Table Indexes Info
+    Database::DbDetailsTableFactory *i = new Database::DbDetailsTableFactory(QStringList() << QObject::tr("Name")
+                                                                                           << QObject::tr("Description")
+                                                                                           << QObject::tr("Column")
+                                                                                           << QObject::tr("Position")
+                                                                                           << QObject::tr("Nullable")
+                                                                                           << QObject::tr("Index Type"));
+    {
+        QSqlQuery query(db);
+        QString   queryString;
+        queryString = QString("SELECT INDEX_NAME, INDEX_COMMENT, COLUMN_NAME, SEQ_IN_INDEX, NULLABLE, INDEX_TYPE FROM INFORMATION_SCHEMA.STATISTICS "
+                              " WHERE TABLE_SCHEMA = '%1' "
+                              "   AND TABLE_NAME = '%2' "
+                              " ORDER BY 1, 4").arg(schemaName).arg(tableName);
+        if (Database::Utils::executeQueryWithLog(&query, queryString))
+            while (query.next()) {
+                QStringList row;
+                row << query.value(0).toString();
+                row << query.value(1).toString();
+                row << query.value(2).toString();
+                row << query.value(3).toString();
+                row << query.value(4).toString();
+                row << query.value(5).toString();
+                i->addRow(row);
+            }
+    }
+
+    // Table Triggers Info
+    Database::DbDetailsTableWithSourceFactory *tr = new Database::DbDetailsTableWithSourceFactory(QStringList() << QObject::tr("Name")
+                                                                                                                << QObject::tr("Event Action")
+                                                                                                                << QObject::tr("Action Timing"));
+    {
+        QSqlQuery query(db);
+        QString   queryString;
+        queryString = QString("SELECT TRIGGER_NAME, EVENT_MANIPULATION, ACTION_TIMING, ACTION_STATEMENT FROM INFORMATION_SCHEMA.TRIGGERS "
+                              " WHERE TRIGGER_SCHEMA = '%1' "
+                              "   AND EVENT_OBJECT_TABLE = '%2' "
+                              " ORDER BY 1").arg(schemaName).arg(tableName);
+        if (Database::Utils::executeQueryWithLog(&query, queryString)) {
+            while ( query.next() )
+                tr->addRow(QStringList() << query.value(0).toString() << query.value(1).toString() << query.value(2).toString(),
+                           query.value(3).toString());
+        }
+    }
+
+    // Table Constraints Info
+    Database::DbDetailsTableFactory *cn = new Database::DbDetailsTableFactory(QStringList() << QObject::tr("Name")
+                                                                                            << QObject::tr("Type"));
+    {
+        QSqlQuery query(db);
+        QString   queryString;
+        queryString = QString("SELECT CONSTRAINT_NAME, CONSTRAINT_TYPE FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS "
+                              " WHERE TABLE_SCHEMA = '%1' "
+                              "   AND TABLE_NAME = '%2' "
+                              " ORDER BY 1").arg(schemaName).arg(tableName);
+        if (Database::Utils::executeQueryWithLog(&query, queryString))
+            while (query.next()) {
+                QStringList row;
+                row << query.value(0).toString();
+                row << query.value(1).toString();
+                cn->addRow(row);
+            }
+    }
+
+    Database::DbDetailsTabbedFactory *tab = new Database::DbDetailsTabbedFactory;
+    tab->addTab(QObject::tr("Table Info"), t);
+    tab->addTab(QObject::tr("Columns Info"), c);
+    tab->addTab(QObject::tr("Indexes"), i);
+    tab->addTab(QObject::tr("Triggers"), tr);
+    tab->addTab(QObject::tr("Constraints"), cn);
+
+    return tab;
+}
+
+static IDbDetailsFactory *createViewDbDetailsFactory(const QString &connName, const QString &schemaName, const QString &tableName)
+{
+    QSqlDatabase db = QSqlDatabase::database(connName);
+
+    if ( !db.isOpen() ) {
+        Core::Storage::warning(QObject::tr("Database connection is not opened."));
+        return nullptr;
+    }
+
+    // View Info
+    Database::DbDetailsFormViewFactory *t = new Database::DbDetailsFormViewFactory;
+    {
+        t->addItem(Database::DbDetailsFormViewFactory::FormItem(QObject::tr("Name"), tableName));
+        QSqlQuery query(db);
+        QString   queryString;
+        queryString = QString("SELECT TABLE_COMMENT FROM INFORMATION_SCHEMA.TABLES "
+                              " WHERE TABLE_SCHEMA = '%1' "
+                              "   AND TABLE_NAME = '%2'").arg(schemaName).arg(tableName);
+        QString description = "";
+        if (Database::Utils::executeQueryWithLog(&query, queryString))
+            if ( query.next() )
+                description = query.value(0).toString();
+        t->addItem(Database::DbDetailsFormViewFactory::FormItem(QObject::tr("Description"), description));
+    }
+
+    // View Columns Info
+    Database::DbDetailsTableFactory *c = new Database::DbDetailsTableFactory(QStringList() << QObject::tr("Name")
+                                                                                           << QObject::tr("Description")
+                                                                                           << QObject::tr("Data Type")
+                                                                                           << QObject::tr("Nullable") );
+    {
+        QSqlQuery query(db);
+        QString   queryString;
+        queryString = QString("SELECT COLUMN_NAME, COLUMN_COMMENT, COLUMN_TYPE, IS_NULLABLE FROM INFORMATION_SCHEMA.COLUMNS "
+                              " WHERE TABLE_SCHEMA = '%1' "
+                              "   AND TABLE_NAME = '%2' "
+                              " ORDER BY ORDINAL_POSITION").arg(schemaName).arg(tableName);
+        if (Database::Utils::executeQueryWithLog(&query, queryString))
+            while (query.next()) {
+                QStringList row;
+                row << query.value(0).toString();
+                row << query.value(1).toString();
+                row << query.value(2).toString();
+                row << query.value(3).toString();
+                c->addRow(row);
+            }
+    }
+
+    Database::DbDetailsTabbedFactory *tab = new Database::DbDetailsTabbedFactory;
+    tab->addTab(QObject::tr("Table Info"), t);
+    tab->addTab(QObject::tr("Columns Info"), c);
+
+    return tab;
+}
+
+static IDbDetailsFactory *createSourceDbDetailsFactory(const QString &connName, const QString &schemaName, const QString &funcName)
+{
+    QSqlDatabase db = QSqlDatabase::database(connName);
+
+    if ( !db.isOpen() ) {
+        Core::Storage::warning(QObject::tr("Database connection is not opened."));
+        return nullptr;
+    }
+
+    QString source = "";
+    QSqlQuery query(db);
+    QString   queryString;
+
+    /// TODO: implement routine parameters and return type for functions from INFORMATION_SCHEMA.PARAMETERS
+    queryString = QString("SELECT ROUTINE_DEFINITION, ROUTINE_TYPE FROM INFORMATION_SCHEMA.ROUTINES "
+                          " WHERE ROUTINE_SCHEMA = '%1' "
+                          "   AND ROUTINE_NAME = '%2'").arg(schemaName).arg(funcName);
+    if (Database::Utils::executeQueryWithLog(&query, queryString))
+        if (query.next()) {
+            source = query.value(1).toString() == "FUNCTION" ? "FUNCTION " : "PROCEDURE ";
+            source += funcName + "\n";
+            source += query.value(0).toString();
+        }
+
+    Database::DbDetailsSourceFactory *s = new Database::DbDetailsSourceFactory(source);
+    return s;
+}
+
+IDbDetailsFactory *Provider::createDbDetailsFactory(const QString &connName, const QString &schemaName, int schemaObjectType, int schemaObjectSubtype, const QString &objName) const
+{
+    Q_UNUSED(schemaName)
+    Q_UNUSED(schemaObjectSubtype)
+
+    switch ( schemaObjectType ) {
+        case Sql::Constants::DbSchemaObjectType::C_TABLE:
+            return createTableDbDetailsFactory(connName, schemaName, objName);
+        case Sql::Constants::DbSchemaObjectType::C_VIEW:
+            return createViewDbDetailsFactory(connName, schemaName, objName);
+        case Sql::Constants::DbSchemaObjectType::C_ROUTINE:
+            return createSourceDbDetailsFactory(connName, schemaName, objName);
+    }
+
+    return nullptr;
 }
 
 #ifndef HAVE_QT5
